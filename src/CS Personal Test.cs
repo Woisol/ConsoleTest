b48724a63ef889acf409619ae0b75820582cb647
Woisol-G <Woisol-G@outlook.com>
2024-01-08 17:03:02 +0800

    终于实现了类Matlab的矩阵输入方式！

--- src/CS Personal Test.cs
+++ src/CS Personal Test.cs
@@ -19,7 +19,6 @@
 // }
 //**EOF 2024-01-06 16:53：千辛万苦第一个程序哈哈
 
-
 //**PT 2024-01-06 18:50
 //**About:关于格式化输入输出
 // using System;
@@ -29,22 +28,141 @@
 // 	{
 // 		// public string inputString;//！在退出构造函数时，不可为 null 的 字段“inputString”必须包含非 null 值。请考虑将 字段 声明为可以为 null。
 // 		// public string inputString = new string;//！“string”不包含采用 0 个参数的构造函数
-// 		public string inputString = "";
+// 		string inputString = "";
 // 		// inputString = Console.ReadLine();//!傻了这里都不是函数你搞个锤
-// 		void input ()
+// 		internal void input ()
 // 		{
 // 			inputString = Console.ReadLine();//！可能的null引用赋值az
 // 		}
-// 		void formatPrintMethod1()
+// 		internal void formatPrintMethod1()
 // 		{
-
+// 			// inputString = string.Format("")//！这个不是…………是用来格式化合并的
+// 			Console.WriteLine("Type into the sentence");
+// 			//！关键！记住它！
+// 			string[] strings = inputString.Split(new char[3] { ' ', ',', ';' });//！这个方法不会删掉两个相邻的分隔…………
+// 			Console.WriteLine("The words you input are list below");
+// 			foreach (string s in strings)
+// 				Console.WriteLine(s.Trim()); //！所以要加这个，咳咳没用，那部分本身就是一个空格了…………
 // 		}
 // 	}
 // 	class MainClass
 // 	{
 // 		static void Main()//！不要漏了static啊！
 // 		{
-
+// 			FormatIO io = new FormatIO();
+// 			io.input();
+// 			io.formatPrintMethod1();
+// 			Console.ReadLine();
+// 			Main();
 // 		}
 // 	}
 // }
+
+//##################################################################################
+//**PT 2024-01-08 10:39
+//**About:关于指针的平替
+// using System;
+
+// using Microsoft.VisualBasic;
+
+// namespace Pointer
+// {
+// 	class PointedClass
+// 	{
+// 		public int value = 0;
+// 	}
+// 	class MainClass
+// 	{
+// 		static void Main()
+// 		{
+// 			// object ClassPtr;
+// 			//!并不是用object
+// 			PointedClass ClassPtr;
+
+// 			PointedClass pointedClass1 = new PointedClass() ;//！无法使用集合初始值设定项初始化类型“PointedClass”，原因是它不实现“System.Collections.IEnumerable”
+// 			PointedClass pointedClass2 = new PointedClass();
+// 			pointedClass1.value = 0;
+// 			pointedClass2.value = 0;
+
+// 			ClassPtr = pointedClass1;
+// 			ClassPtr.value = 1;
+// 			Console.WriteLine(pointedClass1.value);
+
+// 			ClassPtr = pointedClass2;
+// 			ClassPtr.value = 2;
+// 			Console.WriteLine(pointedClass2.value);
+// 			//！好耶证实了！类的 = 就是引用类型！
+// 		}
+// 	}
+// }
+
+//**PT 2024-01-08 12:40
+//##About:关于list
+//！太好用了吧哈哈哈
+// using System;
+// using System.Globalization;
+// using System.Net.Http.Headers;
+// using System.Security.Cryptography.X509Certificates;
+// namespace ListTestSpace
+// {
+// 	class Thing
+// 	{
+// 		public string? content;
+// 		public Thing(String? content)
+// 		{
+// 			this.content = content;//！自动补的，记得要用this！
+// 		}
+// 	}
+// 	class MainClass
+// 	{
+// 		static void Main()
+// 		{
+// 			// List<int> nums = new List<int>();
+// 			// Console.WriteLine("Type a series of numbers, use Enter to show the result");
+// 			// while (true)
+// 			// {
+// 			// 	string? input = Console.ReadLine();
+// 			// 	if (input == "") break;
+// 			// 	// nums.Add((int)input);//！无法将string类型转换为int？？？
+// 			// 	nums.Add(Convert.ToInt32(input));
+// 			// }
+// 			// foreach (var num in nums)
+// 			// 	Console.WriteLine(num);
+
+// 			//**----------------------------好耶！！好用！！！下面试一下类-----------------------------------------------------
+// 			List<Thing> things = new List<Thing>();
+// 			Console.WriteLine("Now enter some string");
+// 			while (true)
+// 			{
+// 				string? input = Console.ReadLine();//！注意还是要再定义一次！上面那个好像也是作用域内的
+// 				if (input == "") break;
+// 				things.Add(new Thing(input));//！补出来的！！！会了！“Thing”不包含采用 1 个参数的构造函数所以只要加了这个就行
+// 											 // things.Add();
+// 			}
+// 			foreach (Thing thing in things)
+// 			{
+// 				Console.WriteLine(thing.content);
+// 			}
+// 			//**----------------------------下标寻找-----------------------------------------------------
+// 			Console.WriteLine("Type the orderNum to find the content.");
+// 			while(true)
+// 			{
+// 				// int? input = Convert.ToInt32(Console.ReadLine());//！即使有下面这一行也会导致空输入错误………………
+// 				// int? input = (var tmpintput = Console.ReadLine()) == ""?Console.WriteLine("Input is empty!"),break:Convert.ToInt32(tmpintput);//！即使有下面这一行也会导致空输入错误………………
+// 				//!不行三目太局限了…………
+// 				string? tmpIntput = Console.ReadLine();
+// 				// if(tmpIntput == null)...
+// 				if(tmpIntput == ""){ Console.WriteLine("Input is empty!"); continue; }//！注意是""而不是null！
+// 				int input = Convert.ToInt32(tmpIntput);
+// 				// if (input == null) break;//！哈哈可空类型
+// 				if (input < 0 || input >= things.Count) Console.WriteLine("Please input a number between 0 and " + (things.Count - 1));
+// 				else
+// 				{
+// 					Console.WriteLine(@"The "+ input.ToString() + "th content is " + things[input].content);//！果然可以这样！！但是要求必须要用int，short不行int?也不行
+// 					// Console.WriteLine("Input is {input}");//！你以为为什么不能输出数字…………都没有tostring啊！而且在里面似乎不能这样搞要用+
+// 				}
+// 			}
+// 		}
+// 	}
+// }
+//**EOF 2024-01-08 13:32：OK基本实现！好玩！！！
--- src/Matrix Calculator.cs
+++ src/Matrix Calculator.cs
@@ -3,7 +3,14 @@
 //**PT 2024-01-07 15:00+
 //**About:实现了转置，搞控制命令还没搞好…………
 //**EOF 2024-01-07 23:16：好晕啊…………我是谁？我现在搞到哪里了？
+//**PT 2024-01-08 12:00+-
+//**About:又忘记heading再写了…………
+//！注意git会用好用但是不会用就会像刚刚15:05差点就新写的输入和list测试代码都丢了…………
+//！原因似乎是在于在git desk那边切换了分支但是又没有commit现有的………………
+//！还好有那个stashed好像恢复了………………
+//td查一下那个stash听说很好用！
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Reflection.Emit;
 using MatrixCalculationApps;//!？？？怎么又出现了/汗是编译器需要加的吗？
@@ -26,7 +33,7 @@ namespace MatrixCalculationApps
 		internal string? name;
 		short length;
 		short height;
-		public int[,]? element;
+		public int[,]? elements;
 		static Matrix curMatrix;
 
 		//**----------------------------DataIO-----------------------------------------------------
@@ -45,64 +52,110 @@ namespace MatrixCalculationApps
 			// Console.WriteLine("Now initing a " + length + "*" + height + " matrix.");
 			Console.WriteLine("Now initing a {length}*{height} matrix.");
 		}
-		// internal void init()//！参数类型void无效…………
-		// {
-		// 	Console.Write("Input the length and height of the matrix(Format: m,n -> a m*n matrix): ");
-		// 	string? tmpInput = Console.ReadLine();//！加一个?就是可空类型了
-		// 	string[] parts = tmpInput.Split(',');
-		// 	length = short.Parse(parts[0].Trim()); //parts[0].Trim();//！无法将strng类型隐式转化为int
-		// 	height = Convert.ToInt16(parts[1].Trim());//！自动补全给出的解决方案
-		// 	//！详解：
-		// 	//!Trim()去掉前后空格
-		// 	//!Parse()将string类型转化为int类型，如果失败则返回0//艹这个注释是自己出来的
-		// 	//!而且这玩意居然是在int下而不是在string下
-		// 	//!Convert.ToInt16()将string类型转化为int类型，如果失败则返回0
-		// 	// length = Int16.Parse(Console.ReadLine());//！你看它这里自己补全的也是不用matrix.的………………
-		// 	// length = Convert.ToInt16(Console.ReadLine());//！你看它这里自己补全的也是不用matrix.的………………
-		// 	// height = Convert.ToInt16(Console.ReadLine());
-		// 	// Console.WriteLine()")//！笑死怎么又帮我写了哈哈不过不是想这样写
-
-		// }
+		//~~ internal void init()//！参数类型void无效…………
+		//~~ {
+		//~~ 	Console.Write("Input the length and height of the matrix(Format: m,n -> a m*n matrix): ");
+		//~~ 	string? tmpInput = Console.ReadLine();//！加一个?就是可空类型了
+		//~~ 	string[] parts = tmpInput.Split(',');
+		//~~ 	length = short.Parse(parts[0].Trim()); //parts[0].Trim();//！无法将strng类型隐式转化为int
+		//~~ 	height = Convert.ToInt16(parts[1].Trim());//！自动补全给出的解决方案
+		//~~ 	//！详解：
+		//~~ 	//!Trim()去掉前后空格
+		//~~ 	//!Parse()将string类型转化为int类型，如果失败则返回0//艹这个注释是自己出来的
+		//~~ 	//!而且这玩意居然是在int下而不是在string下
+		//~~ 	//!Convert.ToInt16()将string类型转化为int类型，如果失败则返回0
+		//~~ 	// length = Int16.Parse(Console.ReadLine());//！你看它这里自己补全的也是不用matrix.的………………
+		//~~ 	// length = Convert.ToInt16(Console.ReadLine());//！你看它这里自己补全的也是不用matrix.的………………
+		//~~ 	// height = Convert.ToInt16(Console.ReadLine());
+		//~~ 	// Console.WriteLine()")//！笑死怎么又帮我写了哈哈不过不是想这样写
+		//~~ }
 		internal void input()
 		{
-			var line = 0;
+			//**----------------------------传统思路，不过有二维list先搞搞看！！-----------------------------------------------------
+			//~~ while(true)
+			//~~ {
+			//~~ 	string? input = Console.ReadLine();
+			//~~ 	if(input == "\n"){}
+			//~~ }
+			//**----------------------------换个思路吧用传统思路好求多了…………！！！尝试用二维list！-----------------------------------------------------
+			// var curLine = 0;
 
-			list<string> input = new list<string>();
-			input.add(console.ReadLine());
-			string[] lineElementString = input[line++].Split(new char[2] { ' ', ',' });
-			length = lineElementString.length;
-			list<int> lineElement = new list<int>();//！不对啊这个也要用list啊！
+			//$处理第一行并获得列数
+			string? tmpintput = Console.ReadLine();
+			string[] lineElementString = tmpintput.Split(new char[2] {' ', ',' });
+			length = (short)lineElementString.Length;
+			int[] curLineElement = new int[length];
 			for(var i = 0;i < length;i++)
-			lineElement[i] = convert.ToInt32(lineElementString[i]);
-			while(true)
+				curLineElement[i] = Convert.ToInt32(lineElementString[i]);
+			List<int[]> tmpElements = new List<int[]>();
+			//！md服了！！！！！！！问通义它看不出来问NB才知道element少了一个s！！！！！！！！
+			//！啊啊啊啊啊啊抽死你艹！！！
+			tmpElements.Add(curLineElement);
+
+			//$处理后面的行
+			while (true)
 			{
-				input.add(Console.ReadLine());
-				lineElementString = input[line++].Split(new char[2] {'', ',' });
-				if(length != input[line].length){console.WriteLine("Elements length not consistent!");line--; continue; }
-				int[] lineElement = new int[length];
+				// tmpElements.Add(curLineElement);//！艹这里还想补全少一个s给我…………原来有一个方法还是什么是elememt的
+				//！不得移到上面还是两个一起改…………
+				curLineElement = new int[length];
+				//！是的NB说了，可以这样重置数组！！！
+				tmpintput = Console.ReadLine();
+				if (tmpintput == "") break;
+				lineElementString = tmpintput.Split(new char[2] {' ', ',' });
+				if((short)lineElementString.Length != length){Console.WriteLine("Elements length not consistent!Retype this line below!");continue; }
 				for(var i = 0;i < length;i++)
-				lineElement[i] = convert.ToInt32()
+					curLineElement[i] = Convert.ToInt32(lineElementString[i]);//！？？？这里居然会把一开始发一起改了？？？
+																			  //！说明这个list可能更多是一个指针！还没有add之前修改是会修改的！
+				// tmpElements.Add((int[])curLineElement.Clone());//td克隆的方法还要查一下
+				tmpElements.Add(curLineElement);
 			}
+
+			//$放入数组中
+			height = (short)tmpElements.Count;//！注意list的长度是用count！
+			elements = new int[height, length];//!这里搞错了…………是先height在length啊！
+			for (var i = 0; i < height; i++)
+			for(var j = 0; j < length; j++)
+				elements[i,j] = tmpElements[i][j];//！[] 内的索引数错误，应为 2，指的是要用[,]而不是[][]!
+
+			//！极度混乱…………在于不知道有快捷的输入数字的方法…………
+			//**----------------------------思路混乱，重写-----------------------------------------------------
+			//~~ var line = 0;
+			//~~ List<string> input = new List<string>();
+			//~~ input.add(Console.ReadLine());
+			//~~ string[] lineElementString = input[line++].Split(new char[2] { ' ', ',' });
+			//~~ length = lineElementString.length;
+			//~~ list<int> lineElement = new list<int>();//！不对啊这个也要用list啊！
+			//~~ for(var i = 0;i < length;i++)
+			//~~ lineElement[i] = convert.ToInt32(lineElementString[i]);
+			//~~ while(true)
+			//~~ {
+			//~~ 	input.add(Console.ReadLine());
+			//~~ 	lineElementString = input[line++].Split(new char[2] {'', ',' });
+			//~~ 	if(length != input[line].length){console.WriteLine("Elements length not consistent!");line--; continue; }
+			//~~ 	int[] lineElement = new int[length];
+			//~~ 	for(var i = 0;i < length;i++)
+			//~~ 	lineElement[i] = convert.ToInt32()
+			//~~ }
 
 			//**---------------------------------------------------------------------------------
 			//！？？？这是什么语法？回头研究一下
-			// while (true)
-			// {
-			// 	try
-			// 	{
-			// 		length = Convert.ToInt16(Console.ReadLine());
-			// 		height = Convert.ToInt16(Console.ReadLine());
-			// 		break;
-			// 	}
-			// 	catch (Exception e)
-			// 	{
-			// 		Console.WriteLine("输入错误，请重新输入！");
-			// 	}
-			// }
+			//~~ while (true)
+			//~~ {
+			//~~ 	try
+			//~~ 	{
+			//~~ 		length = Convert.ToInt16(Console.ReadLine());
+			//~~ 		height = Convert.ToInt16(Console.ReadLine());
+			//~~ 		break;
+			//~~ 	}
+			//~~ 	catch (Exception e)
+			//~~ 	{
+			//~~ 		Console.WriteLine("输入错误，请重新输入！");
+			//~~ 	}
+			//~~ }
 		}
 		internal void input(int length,int height)
 		{
-			element = new int[length, height];//也是自己补全哈哈
+			elements = new int[length, height];//也是自己补全哈哈
 			Console.WriteLine("Input the elements of the "+ length + "*" + height + " matrix: ");
 			string? tmpInput = Console.ReadLine();
 			string[] nums = tmpInput.Split(',');
@@ -110,21 +163,21 @@ namespace MatrixCalculationApps
 			for(var i = 0;i < height;i ++)//!woq vsc在for里面都可以逐句调试我去…………
 			for(var j = 0;j < length;j ++)
 			{
-				element[i, j] = Convert.ToInt16(nums[i * height + j]);//!自动补全的锅…………是nums不是上面的parts啦
+				elements[i, j] = Convert.ToInt16(nums[i * height + j]);//!自动补全的锅…………是nums不是上面的parts啦
 			}
 
-			// for (var i = 0; i < nums.Length; i++)
-			// {
-			// 	element[i / height, i % height] = Convert.ToInt16(nums[i].Trim());
-			// }//！这个方法还不错？！
-			// for (var i = 0; i < element.GetLength(0); i++)
-			// {
-			// 	for (var j = 0; j < element.GetLength(1); j++)
-			// 	{
-			// 		Console.Write("Input the element of the element: ");
-			// 		element[i, j] = Convert.ToInt16(Console.ReadLine());
-			// 	}
-			// }
+			//~~ for (var i = 0; i < nums.Length; i++)
+			//~~ {
+			//~~ 	elements[i / height, i % height] = Convert.ToInt16(nums[i].Trim());
+			//~~ }//！这个方法还不错？！
+			//~~ for (var i = 0; i < elements.GetLength(0); i++)
+			//~~ {
+			//~~ 	for (var j = 0; j < elements.GetLength(1); j++)
+			//~~ 	{
+			//~~ 		Console.Write("Input the element of the element: ");
+			//~~ 		elements[i, j] = Convert.ToInt16(Console.ReadLine());
+			//~~ 	}
+			//~~ }
 			//！笑死这是要僭越了吧哈哈哈
 
 		}
@@ -133,14 +186,14 @@ namespace MatrixCalculationApps
 		//！注意只能由一个params只能标记一维数组而且后面不允许再有其它参数
 		{
 			if(nums.Length != length * height){ Console.WriteLine("Input error!"); return; }
-						element = new int[length, height];//！其实就是漏了这一句
+						elements = new int[length, height];//！其实就是漏了这一句
 			for (var i = 0; i < nums.Length; i++)
 			{
-				element[i / height, i % height] = nums[i];//！这个方法也好用不难记！记它！
+				elements[i / height, i % height] = nums[i];//！这个方法也好用不难记！记它！
 				//！Object reference not set to an instance of an object.
 			}
 			//！！！有没有可能！！！
-			// foreach(var num in element)
+			// foreach(var num in elements)
 			// num = nums[i++];//！想得美，无法为“num”赋值，因为它是“foreach 迭代变量”
 		}
 		internal void print()
@@ -151,46 +204,47 @@ namespace MatrixCalculationApps
 			var i = 0;//！在foreach内static也对该项无效？？？
 			var j = 0;
 				//！隐式类型化的变量不能有多个声明符？？？
-			foreach(var element in element)
+			foreach(var element in elements)
 			{
 				if(i++ == length){ j++; i = 1;Console.WriteLine(); };//！不能在if里面用？？？
 				//!注意这里是i = 1！不然前面i++的就没用了
 				// matrixView[j] = new string[height];//td算了本来想用上格式化输入放到matrixview里面再打印的算了累死了
-				Console.Write(element + " ");
+				Console.Write(element + " ");//！又漏tostring但是一开始明明没有问题的呀
 			}
+			Console.WriteLine();
 			//！用新方法被玩吐了………………
 		}
 		//**----------------------------Operation-----------------------------------------------------
 		internal void Transpose()
 		{
-// 			int[,] tmpNum = new int[length, height];
-			// tmpNum.CopyTo(element,0);//！艹极其方便哈哈哈问TY知道的。后面这个0是开始的下标
-			// for(var j = 0;j < height;j ++)
-			// for(var i = 0;i < length;i ++)
-			// {
-			// 		element[i, j] = tmpNum[j, i];
-			// }
+			//~~ int[,] tmpNum = new int[length, height];
+			//~~ tmpNum.CopyTo(elements,0);//！艹极其方便哈哈哈问TY知道的。后面这个0是开始的下标
+			//~~ for(var j = 0;j < height;j ++)
+			//~~ for(var i = 0;i < length;i ++)
+			//~~ {
+			//~~ 		elements[i, j] = tmpNum[j, i];
+			//~~ }
 			//**----------------------------其实明显还有另一个不用那么多内存的而且可能更快的方法-----------------------------------------------------
-			// for (var j = 0; j < height; j++)
-			// // for(var i = 0, var tmp = 0;i < length;i ++)//！啊用了var不能再for里面定义多个变量？
-			// for(var i = j;i < length;i ++)//！记得这个写法啦！！经典也不难记！
-			// {
-			// 	element[i,j] += element[j, i];
-			// 	element[j, i] = element[i, j] - element[j, i];
-			// 	element[i, j] -= element[j,i];
-			// 	//！这个方法会导致对角线上的元素变成0………………
-			// 	//！而且这玩意万一不是方阵就不对了………………
-			// }
+			//~~ for (var j = 0; j < height; j++)
+			//~~ // for(var i = 0, var tmp = 0;i < length;i ++)//！啊用了var不能再for里面定义多个变量？
+			//~~ for(var i = j;i < length;i ++)//！记得这个写法啦！！经典也不难记！
+			//~~ {
+			//~~ 	elements[i,j] += elements[j, i];
+			//~~ 	elements[j, i] = elements[i, j] - elements[j, i];
+			//~~ 	elements[i, j] -= elements[j,i];
+			//~~ 	//！这个方法会导致对角线上的元素变成0………………
+			//~~ 	//！而且这玩意万一不是方阵就不对了………………
+			//~~ }
 			//**----------------------------方法不对，重写-----------------------------------------------------
 			int[,] newMatrix = new int[height, length];
 			for(var i = 0;i < length;i ++)
 			for(var j = 0;j < height;j ++)
 			{
-				newMatrix[j, i] = element[i, j];
+				newMatrix[j, i] = elements[i, j];
 			}
-			element = new int[height, length];
-			// element.CopyTo(newMatrix,0);//！高维数组不能用这种方法………………
-			Array.Copy(newMatrix, element, newMatrix.Length);
+			elements = new int[height, length];
+			// elements.CopyTo(newMatrix,0);//！高维数组不能用这种方法………………
+			Array.Copy(newMatrix, elements, newMatrix.Length);
 			//！！！高维数组可用这个！！！Array中的静态方法Copy！！！
 		}
 	}
@@ -198,11 +252,11 @@ namespace MatrixCalculationApps
 	class CommandControl
 	{
 		//**----------------------------Matrix Operation-----------------------------------------------------
-		// static Matrix matrix1 = new Matrix();
-		// static Matrix matrix2 = new Matrix();
-		// static Matrix matrix3 = new Matrix();
-		// static Matrix matrix4 = new Matrix();
-		// static Matrix matrix5 = new Matrix();
+		//~~ static Matrix matrix1 = new Matrix();
+		//~~ static Matrix matrix2 = new Matrix();
+		//~~ static Matrix matrix3 = new Matrix();
+		//~~ static Matrix matrix4 = new Matrix();
+		//~~ static Matrix matrix5 = new Matrix();
 
 		static List<Matrix> matrixs = new List<Matrix>();
 
@@ -221,22 +275,22 @@ namespace MatrixCalculationApps
 		//**----------------------------IO-----------------------------------------------------
 		void input()
 		{
-			// while(true)
-			// {
-			// 	Console.WriteLine("Input the command: ");
-			// 	string? input = Console.ReadLine();
-			// 	if(input == "exit") break;
-			// 	else if(input == "transpose") matrix.Transpose();
-			// 	else Console.WriteLine("Command error!");
-			// }//!md又是直接出来哈哈哈哈哈哈
-			// 			while(true)
-			// 			{
-			// 				Console.WriteLine(@"Matrix Calculator
-			// 1. Input the matrix
-			// 2. Transpose the matrix
-			// 3. Exit
-			// Input the command: ");//！记得逐字是@不是$………………
-			// 			}//!也是哈哈哈不过逐字是自己改的
+			//~~ while(true)
+			//~~ {
+			//~~ 	Console.WriteLine("Input the command: ");
+			//~~ 	string? input = Console.ReadLine();
+			//~~ 	if(input == "exit") break;
+			//~~ 	else if(input == "transpose") matrix.Transpose();
+			//~~ 	else Console.WriteLine("Command error!");
+			//~~ }//!md又是直接出来哈哈哈哈哈哈
+			//~~ 			while(true)
+			//~~ 			{
+			//~~ 				Console.WriteLine(@"Matrix Calculator
+			//~~ 1. Input the matrix
+			//~~ 2. Transpose the matrix
+			//~~ 3. Exit
+			//~~ Input the command: ");//！记得逐字是@不是$………………
+			//~~ 			}//!也是哈哈哈不过逐字是自己改的
 			Console.WriteLine(@"Matrix Calculator
 Type ""help"" for Command Support.
 Enjoy it;)");
@@ -292,13 +346,16 @@ Enjoy it;)");
 		static void Main()
 		{
 			//**----------------------------Testing Code-----------------------------------------------------
-			Matrix matrix = new Matrix();//！az主要要用()
-			matrix.init(3,3);
-			matrix.input(1, 2, 3, 4, 5, 6, 7, 8, 9);
-			matrix.Transpose();
-			matrix.print();
+			// Matrix matrix = new Matrix();//！az主要要用()
+			// matrix.init(3,3);
+			// matrix.input(1, 2, 3, 4, 5, 6, 7, 8, 9);
+			// matrix.Transpose();
+			// matrix.print();
 			//**---------------------------------------------------------------------------------
-
+			Matrix matrix = new Matrix();
+			matrix.input();
+			matrix.print();
+			Main();
 
 				// // int[3,2] matrix = { 1, 2, 3, 4, 5, 6 };//！注意不能直接这样声明…………不输入行列说应输入嵌入数组初始值设定值，输入了还是说要用new
 				// int[,] matrix = new int[2,3]{ {1, 2, 3}, {4, 5, 6} };//！注意不能直接这样声明…………不输入行列说应输入嵌入数组初始值设定值，输入了还是说要用new
@@ -313,4 +370,6 @@ Enjoy it;)");
 		}
 	}
 }
-// //**EOF 2024-01-06 21:31：累了，先这样吧，寒假在慢慢完善
+//**EOF 2024-01-06 21:31：累了，先这样吧，寒假在慢慢完善
+//**EOF 2024-01-08 17:01：终于实现啦！！！！！！！类Matlab的矩阵输入！！！！！！！！！！
+//**鬼知道搞了多久呜呜呜
